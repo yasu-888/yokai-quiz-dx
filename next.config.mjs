f482384154e7051f0f21a731c549c09f4fb2f7fc
Yasu-git888 <yasushi.moc.liamg@gmail.com>
2024-09-29 15:42:49 +0900

    fix: add assetPrefix to next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,7 @@
 const nextConfig = {
   output: "export",
   basePath: process.env.NODE_ENV === "production" ? "/yokai-quiz-dx" : "",
+  assetPrefix: process.env.NODE_ENV === "production" ? "/yokai-quiz-dx" : "",
 };
 
 export default nextConfig;
